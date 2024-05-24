be5956d561513549a899b9c59d8855d6be52c33e
Peter Hanukaev <phanukaev@gmail.com>
2024-05-24 18:40:30 -0400

    more testing on workflows and docker publishing

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
 
-console.log("succesfully copiled and running");
+console.log('succesfully copiled and running');
+
+function fib(n : number): number {
+    let [a, b] = [0, 1]
+    for (let i = 0; i < n; i++){
+        [a, b] = [b, a + b];
+    }
+    return a;
+}
+
+console.log('the 20th fibonacci number is ' + fib(20));
